e6363efa924e05d12067e034a13d8605fa58fce1
Master Brickbuilder <Master Brickbuilder@PidgieotoVIII>
2016-11-17 00:18:22 -0700

    Merge origin/master
    Conflicts:
    	src/byui/cit260/saveTheVillage/control/MapControl.java

--- src/byui/cit260/saveTheVillage/model/Spell.java
+++ src/byui/cit260/saveTheVillage/model/Spell.java
@@ -15,21 +15,22 @@ import java.util.Objects;
 public enum Spell implements Serializable {
 
     //Human racial spells are Healing Wind, Fireburst, and Steel Blade
-    HealingWind("Heals the player for a certain amount of health."),
-    Fireburst("Creates a burst of fire dealing a high amount of fire damage to the enemey."),
-    SteelBlade("Increases the base damage of weapon for the next 5 turns."),
+    HealingWind("Heals the player for a meduim amount of health."),
+    Fireburst("Creates a burst of fire dealing high fire damage to "
+        + "the enemey."),
+    SteelBlade("Increases the base damage of weapon for the next 5 turns."), //increase base damage
     //Elf racial spells are Swift Wind, Blinding Light, and Ice Blade
-    SwiftWind("Increases the player's agility for the next 2 turns"),
+    SwiftWind("Increases the player's agility for the next 2 turns"), //increase agility
     BlindingLight("Produces a flash of light, reducing enemy accuracy by 50% for "
-            + "the next turn and 25% for the turn after that."),
-    IceBlade("Deals a medium amount of ice damage to enemy with a 25% chance of "
-            + "freezing the enemy for the next turn."),
+        + "the next turn and 25% for the turn after that."),
+    IceBlade("Deals medium amount of ice damage to enemy with a 25% chance of "
+        + "freezing the enemy for the next turn."),
     //Dwarf racial spells are Earthquake, Iron Body, and Decimating Blow
-    Earthquake("Sacrifice 50 health points to deal high levels of earth damage with "
-            + "a 90% chance of hitting."),
-    IronBody("Increases the player's defense by 50% for the next 2 turns."),
-    DecimatingBlow("Decease player defense by 25% in order to increase attack damage "
-            + "25% for the next 5 turns.");
+    Earthquake("Sacrifice 50 health points to deal  high levels of earth damage "
+        + "with a 90% chance of hitting."), //negative 50 health
+    IronBody("Increases the player's defense by 50% for the next 2 turns."), //defense increase 50%
+    DecimatingBlow("Decease player defense by 25% in order to increase attack "
+        + "damage 25 %for the next 5 turns.");//defense by 25%, attack damage 25%
     
     private final int damageDealt;
     private final int amountHealed;
