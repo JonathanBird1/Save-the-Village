99b182218585c2ff838e48707061dec61f439d8c
Yoda <Yoda@R2d2>
2016-11-21 22:07:52 -0700

    Merge origin/master
    WeaponShopView

--- src/byui/cit260/saveTheVillage/view/WeaponShopView.java
+++ src/byui/cit260/saveTheVillage/view/WeaponShopView.java
@@ -5,7 +5,7 @@
  */
 package byui.cit260.saveTheVillage.view;
 
-import byui.cit260.saveTheVillage.control.PlayerControl;
+import byui.cit260.saveTheVillage.control.GameControl;
 import byui.cit260.saveTheVillage.model.Player;
 import byui.cit260.saveTheVillage.model.Item;
 import byui.cit260.saveTheVillage.control.SceneControl;
@@ -54,8 +54,8 @@ public class WeaponShopView extends View{
         
         //list store inventory 0 to quit 
       System.out.println("#  ITEM\t\tPRICE" );
-  //    String itemArray[]=null;
-
+      String itemArray[]= new String[22];
+   
         int i = 0;
         Item[] items = Item.values();
         for (Item item : items){
@@ -63,7 +63,7 @@ public class WeaponShopView extends View{
               i++;  
               System.out.println(i + "  " + item +"\t" + item.getBuyPrice());
               
-   //           itemArray[i]=item.getItemName();
+              itemArray[i]=item.getItemName();
             }
             
         }
@@ -71,46 +71,51 @@ public class WeaponShopView extends View{
         System.out.println("\nEnter 0 to exit");
         //Prompt for user input of which item to buy
         Scanner keyboard = new Scanner(System.in); //get infile for keyboard
-        String keyboardValue;
-        int value = 0;
+        int keyboardValue = 0;
         boolean valid = false;
-        
-        
+
         while(!valid)
         {
-            keyboardValue = keyboard.nextLine(); //get the next lined entered from keyboard
-            keyboardValue = keyboardValue.trim();
-            int max = 99;//itemArray.length; //get number of items available
-            
+            //get the next int entered from keyboard
+            try {
+                keyboardValue = keyboard.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid item - Leaving shop");
+                return;
+            };
 
-            if(keyboardValue.matches("^\\d+$"))
+            int max = 21; //get number of items available
+
+            if(keyboardValue < 0)
             {
-                System.out.println("Invalid item");
-            }
-            if(value < 0)
-            {
-                System.out.println("Invalid item");
+                System.out.println("Invalid item - Try again");
                 continue;
             }
-            if(value == 0)
+
+            else if(keyboardValue == 0)
             {
                 return;
             }
-            else if(value > max)  //need to get the highest item number
+
+            else if(keyboardValue > max)  //need to get the highest item number
             {
-                System.out.println("Invalid item");
+                System.out.println("Invalid item - Try again");
                 continue;
             }
-
+            else if(itemArray[keyboardValue] == null )
+            {
+                System.out.println("Invalid item - Try again");
+                continue;
+            }
             valid = true;
         }
         
-        
+
         //call the buy item function from SceneControl
-        /*
+        
         SceneControl newSceneControl = new SceneControl();
-        newSceneControl.buyItem(Player, keyboardValue);
-        */
+     //   newSceneControl.buyItem(Player, keyboardValue);  //how do I reference the player
+        
         return; 
         
     }
@@ -118,7 +123,7 @@ public class WeaponShopView extends View{
     private void sellItems() {
         //list store inventory 0 to quit       
         System.out.println("\nList of items to come, for now enter 0 to exit");
-        //Prompt for user input of which item to buy
+        //Prompt for user input of which item to sell
         Scanner keyboard = new Scanner(System.in); //get infile for keyboard
         String keyboardValue;
         int value = 0;
